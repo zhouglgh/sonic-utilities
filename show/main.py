e3bb8b9d75345b79fef49546e43eae4ac654d5ee
Joe LeVeque <jleveque@users.noreply.github.com>
2019-04-30 16:58:57 -0700

    [show] Call teamshow using sudo in 'show interfaces portchannel' (#524)
    * teamshow now also prints message and exits if not run as root

--- show/main.py
+++ show/main.py
@@ -579,7 +579,7 @@ def counters(period, printall, clear, verbose):
 @click.option('--verbose', is_flag=True, help="Enable verbose output")
 def portchannel(verbose):
     """Show PortChannel information"""
-    cmd = "teamshow"
+    cmd = "sudo teamshow"
     run_command(cmd, display_cmd=verbose)
 
 #
